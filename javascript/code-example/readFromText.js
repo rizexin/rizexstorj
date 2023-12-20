ccc8ac4052957634a71aa7cab530565e6a08dd0d
Ilyas Fauzal Akbar <ilyasbit01@gmail.com>
2023-12-20 21:38:58 +0700

    init new

--- javascript/code-example/readFromText.js
+++ javascript/code-example/readFromText.js
@@ -1,12 +1,34 @@
 import fs from 'fs'
 import path from 'path'
+import readline from 'readline'
+
+//define __dirname to current directory
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
+//define source file name
 const sourceFile = 'textExample.txt'
+//concatenate source path with source file name
 const sourcePath = path.join(__dirname, sourceFile)
 
-//stream to read from file and write to console using fs.createReadStream
+async function readFileLineByLine(sourcePath) {
+  // create a read stream
+  const readStream = fs.createReadStream(sourcePath)
+
+  // create readline interface
+  const rl = readline.createInterface({
+    input: readStream,
+    output: process.stdout,
+    terminal: false
+  })
+
+  // read line by line
+  rl.on('line', (line) => {
+    const user = line.split('|')[0]
+    const pass = line.split('|')[1]
+    console.log(`user: ${user} pass: ${pass}`)
+  })
+}
 
 ;(async () => {
-  fs.createReadStream(sourcePath).pipe(process.stdout)
+  await readFileLineByLine(sourcePath)
 })()
